2c9e60e05252bc729f33f3cf7969f3b44a00eea8
Ankita Gupta <ankitagupta94161@gmail.com>
2025-10-20 23:36:40 +0530

    added portal pattern

--- client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
+++ client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
@@ -34,6 +34,7 @@ import CompoundComp from "./ReactNotes/CompoundComp";
 import Props from "./ReactNotes/Props";
 
 import HOC from "./ReactNotes/HOC";
+import PortalPattern from "./ReactNotes/PortalPattern";
 
 
 
@@ -80,13 +81,18 @@ const topicComponents = {
 
 
 
-  "What is react" : <ReactIntroduction/>
+  "What is react" : <ReactIntroduction/>,
 
 
 
 
 
 
+  
+  
+  
+  
+  "Portal Pattern" : <PortalPattern />,
   // add all other topics here
 };
 
