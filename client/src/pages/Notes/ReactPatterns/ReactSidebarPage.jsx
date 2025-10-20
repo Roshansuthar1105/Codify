8c576b7f353e5109b1c46c36d94ee7dd0b0d7679
Ankita Gupta <ankitagupta94161@gmail.com>
2025-10-20 23:27:30 +0530

    added suspense pattern page

--- client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
+++ client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
@@ -34,6 +34,7 @@ import CompoundComp from "./ReactNotes/CompoundComp";
 import Props from "./ReactNotes/Props";
 
 import HOC from "./ReactNotes/HOC";
+import SuspensePattern from "./ReactNotes/SuspensePattern";
 
 
 
@@ -80,13 +81,16 @@ const topicComponents = {
 
 
 
-  "What is react" : <ReactIntroduction/>
+  "What is react" : <ReactIntroduction/>,
 
 
 
 
 
 
+  
+  
+  "Suspense Pattern": <SuspensePattern />  
   // add all other topics here
 };
 
