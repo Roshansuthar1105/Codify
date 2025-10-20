59d7cf0de024235c55a925aaa4c6e6256ebaaa08
Ankita Gupta <ankitagupta94161@gmail.com>
2025-10-20 22:51:39 +0530

    added react hooks factory page

--- client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
+++ client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
@@ -34,6 +34,7 @@ import CompoundComp from "./ReactNotes/CompoundComp";
 import Props from "./ReactNotes/Props";
 
 import HOC from "./ReactNotes/HOC";
+import HooksFactory from "./ReactNotes/HooksFactory";
 
 
 
@@ -45,43 +46,17 @@ const iconMap = {
 };
 const topicComponents = {
   "Introduction to React Patterns": <IntroductionToReactPatterns />,
-
   "What is react" : <ReactIntroduction/>,
   "Provider Pattern": <ProviderComp />,
-
-
-  "What is react" : <ReactIntroduction/>,
   "Layout Component Pattern" : <LayoutComp />,
-
-
-  "What is react" : <ReactIntroduction/>,
   "Compound Components": <CompoundComp />,
-
-
-  "What is react" : <ReactIntroduction/>,
   "Render Props": <Props />,
-
-
-  "What is react" : <ReactIntroduction/>,
   "Higher-Order Components (HOC)": <HOC />,
-
-
-
   "Context API": <ContextAPI />,
-
-
   "Controlled vs Uncontrolled Components": <ControlAndUn />,
-
-
   "State Colocation" : <StateCol />,
-
   "Functional components": <FunctionComp />,
-
-
-
-
-  "What is react" : <ReactIntroduction/>
-
+  "Hooks Factory": <HooksFactory />,
 
 
 
