b6862ab616931bf92ba5c5fc5c55326d6d6e3441
Ankita Gupta <ankitagupta94161@gmail.com>
2025-10-20 22:58:36 +0530

    added state reducer pattern

--- client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
+++ client/src/pages/Notes/ReactPatterns/ReactSidebarPage.jsx
@@ -34,6 +34,7 @@ import CompoundComp from "./ReactNotes/CompoundComp";
 import Props from "./ReactNotes/Props";
 
 import HOC from "./ReactNotes/HOC";
+import StateReducer from "./ReactNotes/StateReducer";
 
 
 
@@ -80,10 +81,8 @@ const topicComponents = {
 
 
 
-  "What is react" : <ReactIntroduction/>
-
-
-
+  "What is react" : <ReactIntroduction/>, 
+  "State Reducer Pattern": <StateReducer />,
 
 
 
